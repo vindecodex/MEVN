e93841db923cd83ce63c42e5d8d467c2204dc361
Vincent Villaluna <villalunavincent@gmail.com>
2020-01-30 19:19:38 +0800

    added src

---
+++ client/src/App.vue
@@ -0,0 +1,76 @@
+<template>
+	<div id="app">
+		<LoginForm />
+	</div>
+</template>
+
+<script>
+import LoginForm from './components/LoginForm.vue'
+
+export default {
+  name: 'app',
+  components: {
+    LoginForm
+  }
+}
+</script>
+
+<style>
+
+/* Predifined */
+.bg-primay {
+	background: #76bbc7;
+	color: #fff;
+}
+.bg-secondary {
+	background: #536564;
+	color: #fff;
+}
+.bg-tertiary {
+	background: #d5dedc;
+}
+
+/* Resets */
+* {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+	font-family: sans-serif;
+}
+input:focus, textarea:focus, select:focus{
+        outline: none;
+}
+
+/* Additionals */
+#app {
+	max-width: 1080px;
+	width: 100%;
+	margin: 0 auto;
+	position: relative;
+	padding: 0 10px;
+}
+
+form {
+	font-size: 18px;
+	width: 100%;
+}
+
+body, html {
+	background: #ececec;
+}
+
+input {
+	padding: 10px 5px;
+	margin: 10px 0;
+	width: 100%;
+	border: 1px solid #fff;
+	border-radius: 8px;
+}
+
+input[type="submit"] {
+	display: block;
+	padding: 10px 5px;
+	margin: 3px 0;
+	cursor: pointer;
+}
+</style>
---
+++ client/src/components/LoginForm.vue
@@ -0,0 +1,40 @@
+<template>
+	<div class="login-container">
+	<h3 class="bg-secondary">Login</h3>
+		<form class="bg-tertiary">
+		<div class="field">
+			<label>Username</label>
+			<input type="text" placeholder="Username">
+		</div>
+		<div class="field">
+			<label>Password</label>
+			<input type="password" placeholder="Password">
+		</div>
+		<input class="bg-primay" type="submit" value="Login">
+		</form>
+	</div>
+</template>
+
+<script>
+export default {
+  name: 'LoginForm',
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.login-container {
+	width: 500px;
+	max-width: 100%;
+	margin: 0 auto;
+	transform: translate(0, 50%);
+}
+.login-container h3 {
+	padding: 10px;
+}
+form {
+	padding: 10px;
+}
+</style>
