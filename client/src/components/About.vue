9d5fdb09fd993d8f2795b46b961391fc04fc7fef
Vincent Villaluna <villalunavincent@gmail.com>
2020-02-15 15:29:46 +0800

    added about component

---
+++ client/src/components/About.vue
@@ -0,0 +1,39 @@
+<template>
+	<div class="login-container">
+		<h3>About</h3>
+		<ul>
+			<li v-for="(rate, index) in ratings" :key="index">{{ rate }}</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+	name: 'About',
+	props: {
+	},
+	data() {
+		return {
+		ratings: 0
+		}
+	},
+	mounted() {
+		axios.get('/api')
+		.then(res => { 
+			this.ratings = res.data.message.data;
+			console.log(res.data.message.data);
+		})
+		.catch(err => { 
+			console.log(err);
+		});
+	}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+li {
+	list-style-type: none;
+}
+</style>
