9b650963152061bc0351275fd229308f74fddb3f
Vincent Villaluna <villalunavincent@gmail.com>
2020-02-15 15:29:29 +0800

    added 404 component

---
+++ client/src/components/404.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="login-container">
+		<h3>404 Not Found</h3>
+	</div>
+</template>
+
+<script>
+export default {
+  name: 'Four0Four',
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
