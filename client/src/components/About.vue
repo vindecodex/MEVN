716ae715d9f3087eeb0483d2654241f3fe780c12
Vincent Villaluna <villalunavincent@gmail.com>
2020-02-15 16:20:06 +0800

    fix about component

--- client/src/components/About.vue
+++ client/src/components/About.vue
@@ -1,39 +1,24 @@
 <template>
 	<div class="login-container">
-		<h3>About</h3>
-		<ul>
-			<li v-for="(rate, index) in ratings" :key="index">{{ rate }}</li>
-		</ul>
+		<h3>What is Lorem Ipsum?</h3>
+		<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
 	</div>
 </template>
 
 <script>
-import axios from 'axios';
 export default {
 	name: 'About',
 	props: {
 	},
 	data() {
 		return {
-		ratings: 0
 		}
 	},
 	mounted() {
-		axios.get('/api')
-		.then(res => { 
-			this.ratings = res.data.message.data;
-			console.log(res.data.message.data);
-		})
-		.catch(err => { 
-			console.log(err);
-		});
 	}
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-li {
-	list-style-type: none;
-}
 </style>
